31c4310a8dac015c769906bf8af5ee89c2374671
wladimirfilho <wladimir.wf@gmail.com>
2025-01-13 14:14:30 +1100

    fix for deploy

--- components/shared/header/mode_toggle.tsx
+++ components/shared/header/mode_toggle.tsx
@@ -4,8 +4,6 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
 } from "@/components/ui/dropdown-menu";
--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { use, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
